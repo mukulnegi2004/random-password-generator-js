c3819c56341d96f6eaf1bb025fcda1d641f71ef4
Mukul Negi <mannunegi126@gmail.com>
2025-07-12 02:40:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,21 @@ const allchars = uppercase + lowercase + number + symbol;
 
 function createpassword(){
     let password = "";
-    password += uppercase[Math.floor(Math.random()*uppercase.length)];   
+    password += uppercase[Math.floor(Math.random()*uppercase.length)];  
     password += lowercase[Math.floor(Math.random()*lowercase.length)];
     password += number[Math.floor(Math.random()*number.length)];
     password += symbol[Math.floor(Math.random()*symbol.length)];
 
-    while(password.length < length){  
+    while(password.length < length){   
         password += allchars[Math.floor(Math.random()*allchars.length)];
     }
     passwordbox.value =  password;
 }
 
 function copypassword(){
-    passwordbox.select();  
-    document.execCommand("copy");   
+    passwordbox.select(); 
+    document.execCommand("copy");  
+    setTimeout(() => {
         alert("Password copied to clipboard!");
     },1000);
 }
